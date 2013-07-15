982fff15f02bfbafb0d0852e64918f48bad45ed5
Wesley Carr <surfswell17@gmail.com>
2013-07-15 10:38:34 -0400

    Created tweets table

---
+++ db/migrate/20130715143425_create_tweet.rb
@@ -0,0 +1,14 @@
+class CreateTweet < ActiveRecord::Migration
+  def up
+    create_table :tweets do |t|
+      t.string :text
+      t.belongs_to :user
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :tweets
+  end
+end
