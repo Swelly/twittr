101e8391a7037d725739cd00c5483b1d773f4b83
Wesley Carr <surfswell17@gmail.com>
2013-07-15 10:33:45 -0400

    Init. Created user database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twittr::Application.config.secret_token = '024763b2e0faf266698e247828b617066e80cad99965546b5acde3cce00b28a74b12cf864f8eaaee14568c0788c8e6976b9907ee6b432648966be4199b541e9f'
