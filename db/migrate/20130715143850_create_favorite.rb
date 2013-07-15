6d468fd1e344bde0ee04f60e93630894d70ed4c0
Wesley Carr <surfswell17@gmail.com>
2013-07-15 10:42:36 -0400

    Created favorites table

---
+++ db/migrate/20130715143850_create_favorite.rb
@@ -0,0 +1,14 @@
+class CreateFavorite < ActiveRecord::Migration
+  def up
+    create_table :favorites do |t|
+      t.belongs_to :user
+      t.belongs_to :tweet
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :favortes
+  end
+end
